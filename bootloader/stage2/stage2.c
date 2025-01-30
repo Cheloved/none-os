2dbaf8c7c9de7e3c29940ac40b08e6956e623698
Cheloved <27046990+Cheloved@users.noreply.github.com>
2025-01-30 19:08:46 +0300

    Small fixes

--- bootloader/stage2/stage2.c
+++ bootloader/stage2/stage2.c
@@ -7,22 +7,28 @@ void main()
     char* text = "Stage 2 (C) executed\r\n";
     puts(text);
 
+    // Чтение информации о VBE
     VBEInfo vbeinfo;
     if ( read_vbeinfo (&vbeinfo) != 0x004F )
-    {
         puts(" [Error] while reading VBE info\r\n");
-    }
 
-    uint16_t* mode_list = (uint16_t*)(vbeinfo.video_modes_ptr);
+    // Проход по всем доступным режимам
     for ( uint16_t* mode_list = (uint16_t*)(vbeinfo.video_modes_ptr);
           *mode_list != 0xFFFF; mode_list++)
     {
+        // Получение информации о режиме
         VBEModeInfo modeinfo;
         read_mode_info(*mode_list, &modeinfo);
 
+        // Если не 32 бита на пиксель, переходим к следующему
         if ( modeinfo.bits_per_pixel != 32 )
             continue;
 
+        // Если не линейный буффер, переходим к следующему
+        if ( !(modeinfo.attributes & 0x80) )
+            continue;
+
+        // Вывод основной информации о режиме
         puts("Mode 0x");
         puthex16(*mode_list);
         puts(" --- Resolution: ");
